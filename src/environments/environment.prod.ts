9c08d494af113df6a198879a5aa16965eca2aff9
webcurso <45634714+webcurso@users.noreply.github.com>
2018-12-06 19:43:46 -0300

    añadimos firebaseconfig

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+  	apiKey: "AIzaSyD0xgi5n3MmQGjEM3puS9FXpH9fiao_t_Q",
+    authDomain: "angel-15748.firebaseapp.com",
+    databaseURL: "https://angel-15748.firebaseio.com",
+    projectId: "angel-15748",
+    storageBucket: "angel-15748.appspot.com",
+    messagingSenderId: "643139375946"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+  	apiKey: "AIzaSyD0xgi5n3MmQGjEM3puS9FXpH9fiao_t_Q",
+    authDomain: "angel-15748.firebaseapp.com",
+    databaseURL: "https://angel-15748.firebaseio.com",
+    projectId: "angel-15748",
+    storageBucket: "angel-15748.appspot.com",
+    messagingSenderId: "643139375946"
+  }
 };
 
 /*
